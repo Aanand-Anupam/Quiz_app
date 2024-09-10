58c2647660b08fc028084fcf6f8777fd56987c14
Aanand-Anupam <Anupamaanandthakur@gmail.com>
2024-09-10 23:38:40 +0530

    Fifth commit

--- script.js
+++ script.js
@@ -29,105 +29,61 @@ const diff_level = document.querySelector("input[type=radio]:checked");
 
 
 
-function startGame(){
+function createRadioOption(id, value, className) {
+    // Create the div container for the option
+    const div = document.createElement("div");
+    div.setAttribute("class", "form-check");
+
+    // Create the input element
+    const input = document.createElement("input");
+    input.setAttribute("class", "form-check-input");
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "exampleRadios");
+    input.setAttribute("id", id);
+    input.setAttribute("value", value);
+
+    // Create the label element
+    const label = document.createElement("label");
+    label.setAttribute("class", "form-check-label");
+    label.setAttribute("for", id);
+    label.setAttribute("class", className);
+
+    // Append the input and label to the div
+    div.append(input, label);
+
+    return div;
+}
+
+function startGame() {
+   
     const form = document.createElement("form");
     form.setAttribute("class", "form");
     container.append(form);
+
     const ques = document.createElement("h3"); 
     ques.setAttribute("class", "question");
     form.append(ques);
-    //Parent div:
-    
 
-    const div_option1 = document.createElement("div");
-    div_option1.setAttribute("class", "form-check");
-    form.append(div_option1);
-    //Making input:
-    const input1 = document.createElement("input");
-    input1.setAttribute("class", "form-check-input");
-    input1.setAttribute("type", "radio");
-    input1.setAttribute("name", "exampleRadios");
-    input1.setAttribute("id", "exampleRadios1");
-    input1.setAttribute("value", "option1");
-    div_option1.append(input1);
+    // Append all options using the helper function
+    const options = [
+        { id: "exampleRadios1", value: "option1", className: "opt_1" },
+        { id: "exampleRadios2", value: "option2", className: "opt_2" },
+        { id: "exampleRadios3", value: "option3", className: "opt_3" },
+        { id: "exampleRadios4", value: "option4", className: "opt_4" }
+    ];
 
-    const label_1 = document.createElement("LABEL");
-    label_1.setAttribute("class", "form-check-label");
-    label_1.setAttribute("for", "exampleRadios1");
-    label_1.setAttribute("class", "opt_1");
-    
-    div_option1.append(label_1);
+    options.forEach(option => {
+        form.append(createRadioOption(option.id, option.value, option.className));
+    });
 
-    //Parent div:
-    const div_option2 = document.createElement("div");
-    div_option2.setAttribute("class", "form-check");
-    form.append(div_option2);
-    //Making input:
-    const input2 = document.createElement("input");
-    input2.setAttribute("class", "form-check-input");
-    input2.setAttribute("type", "radio");
-    input2.setAttribute("name", "exampleRadios");
-    input2.setAttribute("id", "exampleRadios2");
-    input2.setAttribute("value", "option2");
-    div_option2.append(input2);
-
-    const label_2 = document.createElement("LABEL");
-    label_2.setAttribute("class", "form-check-label");
-    label_2.setAttribute("for", "exampleRadios2");
-    label_2.setAttribute("class", "opt_2");
-    
-    div_option2.append(label_2);
-
-    //Option3:
-
-    //Parent div:
-    const div_option3 = document.createElement("div");
-    div_option3.setAttribute("class", "form-check");
-    form.append(div_option3);
-    //Making input:
-    const input3 = document.createElement("input");
-    input3.setAttribute("class", "form-check-input");
-    input3.setAttribute("type", "radio");
-    input3.setAttribute("name", "exampleRadios");
-    input3.setAttribute("id", "exampleRadios3");
-    input3.setAttribute("value", "option3");
-    div_option3.append(input3);
-
-    const label_3 = document.createElement("LABEL");
-    label_3.setAttribute("class", "form-check-label");
-    label_3.setAttribute("for", "exampleRadios3");
-    label_3.setAttribute("class", "opt_3");
-    
-    div_option3.append(label_3);
-
-    //Option_4:
-    //Parent div:
-    const div_option4 = document.createElement("div");
-    div_option4.setAttribute("class", "form-check");
-    form.append(div_option4);
-    //Making input:
-    const input4 = document.createElement("input");
-    input4.setAttribute("class", "form-check-input");
-    input4.setAttribute("type", "radio");
-    input4.setAttribute("name", "exampleRadios");
-    input4.setAttribute("id", "exampleRadios4");
-    input4.setAttribute("value", "option4");
-    div_option4.append(input4);
-
-    const label_4 = document.createElement("LABEL");
-    label_4.setAttribute("class", "form-check-label");
-    label_4.setAttribute("for", "exampleRadios4");
-    label_4.setAttribute("class", "opt_4");
-    
-    div_option4.append(label_4);
-
-    const btn_2 = document.createElement("button");
-    const text = document.createTextNode("Submit");
-    btn_2.append(text);
-    btn_2.setAttribute("type","submit")
-    btn_2.setAttribute("class","btn btn-primary btn_2");
-    container.append(btn_2);
+    // Create and append the submit button
+    const btn = document.createElement("button");
+    btn.setAttribute("type", "submit");
+    btn.setAttribute("class", "btn btn-primary btn_2");
+    btn.textContent = "Submit";
+    container.append(btn);
 }
+
 
 let i = 0;
 function Playgame(result,i){
@@ -139,13 +95,39 @@ function Playgame(result,i){
     const option2 = document.querySelector(".opt_2");
     const option3 = document.querySelector(".opt_3");
     const option4 = document.querySelector(".opt_4");
-    option1.innerHTML = result.results[i].correct_answer;
-    option2.innerHTML = result.results[i].incorrect_answers[0];
-    if(result.results[i].type == "multiple"){
-        option3.innerHTML = result.results[i].incorrect_answers[1];
-        option4.innerHTML = result.results[i].incorrect_answers[2];
+    //Creating Random Options:
+    const correct_answer = result.results[i].correct_answer;
+    const incorrect_answers = result.results[i].incorrect_answers;
+    let avl_option = [correct_answer, incorrect_answers[0], incorrect_answers[1], incorrect_answers[2]];
+    function shuffleArray(avl_option) {
+        if(result.results[i].type === "boolean"){ 
+            avl_option.splice(2, 2)
+            option3.style.display = "none";
+            option4.style.display = "none";
+        };
+            
+        
+        for (let i = avl_option.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [avl_option[i], avl_option[j]] = [avl_option[j], avl_option[i]]; // Swap elements
+        }
+        return avl_option;
     }
+    avl_option = shuffleArray(avl_option);
+    
+
+   
+    option1.innerHTML = avl_option[0];
+    option2.innerHTML = avl_option[1];
+    if(result.results[i].type === "multiple"){
+        option3.style.display = "block";
+        option4.style.display = "block";
+        option3.innerHTML = avl_option[2];
+        option4.innerHTML = avl_option[3];
+    }
+    
     const btn_2 = document.querySelector(".btn_2");
+    btn_2.removeEventListener("click",func1);
     btn_2.addEventListener("click", func1)
     function func1() {
         i = i+1;
